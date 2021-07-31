e0241df1d6a56c33352c57332eadaddc2dcede6c
Diptam Sarkar <diptamsarkar@gmail.com>
2021-07-30 22:31:11 -0500

    Added button event

--- js/datatable.js
+++ js/datatable.js
@@ -1,5 +1,6 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
+    $('#datatable').hide();
 //     const datatablesSimple = document.getElementById('datatable');
     
 //    table = new simpleDatatables.DataTable(datatablesSimple, {
@@ -10,33 +11,37 @@ $(document).ready(function() {
 //    });
 
     // table.columns.adjust().responsive.recalc();
+    $("#tableBtn").click(function () {
+        console.log("Button clicked")
+        $.ajax({
+            type: "GET",
+            url: "popular_before_90s_tabledata.csv",
+            dataType: "text",
+            success: function (response) {
+                $('#datatable').find('tbody').empty();
+                //console.log(response)
+                let output = response.split("\r\n");
+                output.forEach(element => {
+                    //console.log(element)
+                    let elementArray = element.split(",")
     
-    $.ajax({
-        type: "GET",
-        url: "popular_before_90s_tabledata.csv",
-        dataType: "text",
-        success: function (response) {
-            $('#datatable').find('tbody').empty();
-            //console.log(response)
-            let output = response.split("\r\n");
-            output.forEach(element => {
-                //console.log(element)
-                let elementArray = element.split(",")
-
-                $('#datatable tbody').append("<tr><td>" + elementArray[0] +
-                                            "</td><td>" + elementArray[1] + 
-                                            "</td><td>" + elementArray[2] + 
-                                            "</td><td>" + elementArray[3] + 
-                                            "</td><td>" + elementArray[4] + 
-                                            "</td><td>" + elementArray[5] + 
-                                            "</td><td>" + elementArray[6] + 
-                                            "</td><td>" + elementArray[7] +
-                                            "</td><td>" + elementArray[8] +
-                                            "</td></tr>");
-            });
-            //console.log(JSON.stringify(response))
-        }
+                    $('#datatable tbody').append("<tr><td>" + elementArray[0] +
+                                                "</td><td>" + elementArray[1] + 
+                                                "</td><td>" + elementArray[2] + 
+                                                "</td><td>" + elementArray[3] + 
+                                                "</td><td>" + elementArray[4] + 
+                                                "</td><td>" + elementArray[5] + 
+                                                "</td><td>" + elementArray[6] + 
+                                                "</td><td>" + elementArray[7] +
+                                                "</td><td>" + elementArray[8] +
+                                                "</td></tr>");
+                                                $('#datatable').show();
+                });
+                //console.log(JSON.stringify(response))
+            }
+        });
     });
+    
 
     // const datatablesSimple = document.getElementById('datatable');
     // if (datatablesSimple) {
