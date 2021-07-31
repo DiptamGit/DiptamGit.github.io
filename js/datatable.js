06ff702addd68ba60a5d1add37d185852f6b85a4
Diptam Sarkar <diptamsarkar@gmail.com>
2021-07-30 22:35:07 -0500

    added console logs

--- js/datatable.js
+++ js/datatable.js
@@ -19,13 +19,23 @@ $(document).ready(function() {
             dataType: "text",
             success: function (response) {
                 $('#datatable').find('tbody').empty();
-                //console.log(response)
+                console.log(response)
                 let output = response.split("\r\n");
                 output.forEach(element => {
-                    //console.log(element)
+                    console.log(element)
                     let elementArray = element.split(",")
     
                     $('#datatable tbody').append("<tr><td>" + elementArray[0] +
+                                                "</td><td>" + elementArray[1] + 
+                                                "</td><td>" + elementArray[2] + 
+                                                "</td><td>" + elementArray[3] + 
+                                                "</td><td>" + elementArray[4] + 
+                                                "</td><td>" + elementArray[5] + 
+                                                "</td><td>" + elementArray[6] + 
+                                                "</td><td>" + elementArray[7] +
+                                                "</td><td>" + elementArray[8] +
+                                                "</td></tr>");
+                                                console.log("<tr><td>" + elementArray[0] +
                                                 "</td><td>" + elementArray[1] + 
                                                 "</td><td>" + elementArray[2] + 
                                                 "</td><td>" + elementArray[3] + 
